5c48da2b541d6ebf12c2d154537056a97d2debda
ilsur-hunter00 <ilsur.vafin.00@yandex.ru>
2021-02-06 16:12:19 +0300

    Python projects

---
+++ LAB2.py
@@ -0,0 +1,16 @@
+import math
+
+R1 = int(input("R1: "))
+R2 = int(input("R2: "))
+
+R1 > R2
+
+S1 = math.pi*R1**2
+S2 = math.pi*R2**2
+S3 = S1 - S2
+
+print(S1, S2, S3)
+
+
+
+
---
+++ LAB4.py
@@ -0,0 +1,9 @@
+A = int(input("A: "))
+B = int(input("A: "))
+A > B
+Lenght_1 = A - B
+Count = Lenght_1 // B
+print(Count)
+
+
+
