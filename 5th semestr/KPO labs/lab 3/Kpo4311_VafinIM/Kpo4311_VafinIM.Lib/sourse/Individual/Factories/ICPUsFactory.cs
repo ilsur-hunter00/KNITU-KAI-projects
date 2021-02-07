9cd03687417f4cdbdcd713acbbb29694bc6640a1
ilsur-hunter00 <71671226+ilsur-hunter00@users.noreply.github.com>
2021-02-07 16:26:37 +0300

    Add files via upload

---
+++ 5th semestr/KPO labs/lab 3/Kpo4311_VafinIM/Kpo4311_VafinIM.Lib/sourse/Individual/Factories/ICPUsFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kpo4311.VafinIM.Lib
+{
+    public interface ICPUsFactory
+    {
+        ICPUsListSaver CreateCPUsListSaver();
+        ICPUsListLoader CreateCPUsListLoader();
+    }
+}
